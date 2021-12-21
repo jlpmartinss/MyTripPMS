bba8ef58b45661751d449342be177fefbe2404c7
Francisc546 <49655530+Francisc546@users.noreply.github.com>
2021-12-21 14:05:02 +0000

    Alterações Species Screen (Marcos)

--- src/screens/SpeciesScreen.js
+++ src/screens/SpeciesScreen.js
@@ -1,24 +1,52 @@
 import React from "react";
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, ScrollView, StyleSheet, Image} from 'react-native';
 
 export default function SpeciesScreen(){
+    
     return(
         <View style ={styles.container}>
-            <Text> Species Screen </Text>
-            <Button
-                title="Click here"
-                onPress={()=> alert('Button Cliked!')}
-            />
+            <ScrollView>                
+                <View style ={styles.item}>
+                    <Text style = {styles.item}>Whales</Text>
+                        <Image style = {styles.image} source={require("../../assets/586396-dophin-animals.jpg")}/>
+                </View>
+                <View style ={styles.item}>
+                    <Text style = {styles.item}>Dolphins</Text>
+                        <Image style = {styles.image} source={require("../../assets/586396-dophin-animals.jpg")}/>
+                </View>
+                <View style ={styles.item}>
+                    <Text style = {styles.item}>Seabirds</Text>
+                        <Image style = {styles.image} source={require("../../assets/586396-dophin-animals.jpg")}/>
+                </View>
+                <View style ={styles.item}>
+                    <Text style = {styles.item}>Others</Text>
+                        <Image style = {styles.image} source={require("../../assets/586396-dophin-animals.jpg")}/>
+                </View>                                
+            </ScrollView>
         </View>
     );
-
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: 'cyan',
+        //alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#8fcbbc'
     },
+    item: {
+        flex:1,
+        marginTop: 10,
+        padding: 0,
+        backgroundColor: 'pink',
+        fontSize: 24,
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+        height: 300,
+        resizeMode: 'stretch',
+        backgroundColor: 'white'
+    }
 })
