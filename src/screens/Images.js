1eb7688f490e4af925dab08ab92bf32ebe7c29d4
jlpmartins98 <50104175+jlpmartins98@users.noreply.github.com>
2021-12-23 01:41:24 +0000

    criado para guardar os url das Images, talvez usar só o json do marco

---
+++ src/screens/Images.js
@@ -0,0 +1,8 @@
+export default Images = [
+    {url: require("../../assets/586396-dophin-animals.jpg")},
+    {url: require("../../assets/whale.jpg")},
+    {url: require("../../assets/whale.jpg")},
+    {url: require("../../assets/whale.jpg")},
+    {url: require("../../assets/whale.jpg")},
+
+]
