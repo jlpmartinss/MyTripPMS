de20da8b4932cb1b442476c6b3b2641c23df16f6
jlpmartins98 <50104175+jlpmartins98@users.noreply.github.com>
2021-12-23 01:42:33 +0000

    retirei o flatlist, usei um map para as imagens

--- src/screens/WhalesScreen.js
+++ src/screens/WhalesScreen.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, FlatList, Dimensions, TouchableOpacity } from 'react-native';
-import ListItem from "../../ListItem";
-import { createStackNavigator } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, Button, StyleSheet, FlatList, Dimensions, TouchableOpacity, Image, ImageBackground, ScrollView } from 'react-native';
+import Images from "./Images"
 
-const ITEM_WIDTH = Dimensions.get('window').width
+
+let ITEM_WIDTH = Dimensions.get('window').width;
+let ITEM_HEIGHT = Dimensions.get('window').height;
 //import Whale1 from './Whale1';
 
 //const WhalesStack = createStackNavigator();
 
+const WhalesScreen = ({ navigation }) => {
+    //state = { columns: 2 }
 
 
-export default class WhalesScreen extends React.Component {
-    state = { columns: 2 }
-
-
-    render() {
-        const { navigation } = useNavigation;
-        const { columns } = this.state
-        return (
+    //render() {
+    // const { navigation } = useNavigation;
+    // const { columns } = this.state
+    return (
+        <ScrollView>
             <View style={styles.container}>
-                <FlatList
+                {/* <FlatList
                     numColumns={columns}
                     data={[
                         require("../../assets/whale.jpg"),
@@ -44,32 +43,45 @@ export default class WhalesScreen extends React.Component {
                     keyExtractor={
                         (index) => { return index }
                     }
-                />
-                <Button title="whale1" onPress={() => this.props.navigation.navigate("Whale1")} />
-                <TouchableOpacity style={styles.container}
+                /> */
+                    Images.map((image, index) => (
+                        <TouchableOpacity key={index} onPress={() => navigation.navigate('Whale1')}>
+                            <Image source={image.url} style={{ height: ITEM_HEIGHT / 3, width: ITEM_WIDTH / 3 - 6, borderRadius: 10, margin: 2 }} />
+
+                        </TouchableOpacity>
+                    ))
+
+                }
+                {/* <TouchableOpacity style={styles.container}
                     onPress={() => this.props.navigation.navigate('Whale1')}>
-                    <Text style={styles.text}> ddwd </Text> 
-                </TouchableOpacity>
+                    <Text style={styles.button}> ddwd </Text>
+                </TouchableOpacity> */}
             </View >
-        );
-
-
-    }
-
-
+        </ScrollView>
+    );
 }
-
+//}
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: '#8fcbbc'
+        display: "flex",
+        flexDirection: 'row',
+        flexWrap: 'wrap'
     },
     text: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'red',
+        color: 'white',
+        fontSize: 28,
+        marginHorizontal: 10,
+        marginVertical: 1,
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowRadius: 8
     },
+    button: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+    }
 
 })
+
+export default WhalesScreen
