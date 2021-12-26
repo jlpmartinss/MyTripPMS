f74416d6f38c1262982c63fd02bc298c91c8f647
Marcos <marcos.dss92@gmail.com>
2021-12-26 16:15:39 +0000

    images on whales and dolphoins now work

--- src/Images.js
+++ src/Images.js
@@ -1,8 +1,42 @@
 export default Images = {
+
+    //Trips images
     trip0: require("../assets/586396-dophin-animals.jpg"),
     trip1: require("../assets/whale.jpg"),
     trip2: require("../assets/whale.jpg"),
     trip3: require("../assets/whale.jpg"),
     trip4: require("../assets/whale.jpg"),
+
+    //Dolphins images
+    dolphin1: require("../assets/Dolphins/1Atlantic_spotted_dolphin.jpg"),
+    dolphin2: require("../assets/Dolphins/2Bottlenose_dolphin.jpg"),
+    dolphin3: require("../assets/Dolphins/3Common_dolphin.jpg"),
+    dolphin4: require("../assets/Dolphins/4Frasers_dolphin.jpg"),
+    dolphin5: require("../assets/Dolphins/5Rissos_Dolphin.jpg"),
+    dolphin6: require("../assets/Dolphins/6Rough_toothed_dolphin.jpg"),
+    dolphin7: require("../assets/Dolphins/7Stripped_dolphin.jpg"),
+
+    //Whales images
+    whale1:  require("../assets/Whales/1Blainvilles_beaked_whale.jpg"),
+    whale2:  require("../assets/Whales/2Blue_Whale.jpg"),
+    whale3:  require("../assets/Whales/3Brydes_Whale.jpg"),
+    whale4:  require("../assets/Whales/4Cuviers_beaked_whale.jpg"),
+    whale5:  require("../assets/Whales/5False_killer_whale.jpg"),
+    whale6:  require("../assets/Whales/6Fin_Whale.jpg"),
+    whale7:  require("../assets/Whales/7Gervaiss_beaked_whale.jpg"),
+    whale8:  require("../assets/Whales/8Harbour_porpoise_whale.png"),
+    whale9:  require("../assets/Whales/9humpback_whale.jpg"),
+    whale10: require("../assets/Whales/10Killer_whale_or_Orca.jpg"),
+    whale11: require("../assets/Whales/11Long-finned_pilot_whale.jpg"),
+    whale12: require("../assets/Whales/12Melon-headed_whale.jpg"),
+    whale13: require("../assets/Whales/13Minke_Whale.jpg"),
+    whale14: require("../assets/Whales/14North_Atlantic_Right_Whale.jpg"),
+    whale15: require("../assets/Whales/15Northern_bottlenose_whale.jpg"),
+    whale16: require("../assets/Whales/16Sei_Whale.jpg"),
+    whale17: require("../assets/Whales/17Short-finned_pilot_whale.jpg"),
+    whale18: require("../assets/Whales/18Sowerbys_beaked_whale.jpg"),
+    whale19: require("../assets/Whales/19Sperm_Whale.jpg"),
+    whale20: require("../assets/Whales/20Trues_beaked_whale.jpg")  
+    
 
 }
--- src/screens/Dolphin1.js
+++ src/screens/Dolphin1.js
@@ -1,8 +1,8 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { View, Text, StyleSheet, Dimensions, Image, Button } from 'react-native';
 import { ScrollView } from "react-native-gesture-handler";
 import ImageBackground from "react-native/Libraries/Image/ImageBackground";
+import Images from "../Images";
 
 const ITEM_WIDTH = Dimensions.get('window').width
 
@@ -19,36 +19,62 @@ export default function Dolphin1({ route, navigation}) {
     const length        = data.length;
     const weight        = data.weight;
     const color         = data.color;
-    const imgPath       = data.imgPath;
+    var imgPath         = "";
 
     console.log("I'm in Dolphin1: " + id + " " + name);
+
+    switch (id){
+        case '1':
+            imgPath = Images.dolphin1;
+            break;
+        case '2':
+            imgPath = Images.dolphin2;
+            break;
+        case '3':
+            imgPath = Images.dolphin3;
+            break;
+        case '4':
+            imgPath = Images.dolphin4;
+            break;
+        case '5':
+            imgPath = Images.dolphin5;
+            break;
+        case '6':
+            imgPath = Images.dolphin6;
+            break;
+        case '7':
+            imgPath = Images.dolphin7;
+
+        default:
+            console.log("something went wrong with image id!!!!");
+    }
 
     return (
         
         <View style={styles.container}>
-                <ImageBackground blurRadius = {50} source={require("../../assets/Dolphins/2Bottlenose_dolphin.jpg")} resizeMode="cover" style={styles.imageBackground}>            
+                <ImageBackground blurRadius = {50} source={imgPath} resizeMode="cover" style={styles.imageBackground}>            
                 <ScrollView>
                     <Text style={styles.textTitle}>{name}</Text>
                     <Text style={styles.textNormal, styles.textCientific}>Cientific name: {cientificName}</Text>  
-                        <Image style={styles.image} source={require("../../assets/Dolphins/2Bottlenose_dolphin.jpg")}/>
-                            
-                        {summary != ""? <Text style={styles.textSubTitle}>Summary:</Text>:null}
-                            {summary != ""? <Text style={styles.textNormal}>{summary}</Text>:null}
+                    <Image style={styles.image} source={imgPath}/>
+                        
+                    {summary != ""? <Text style={styles.textSubTitle}>Summary:</Text>:null}
+                        {summary != ""? <Text style={styles.textNormal}>{summary}</Text>:null}
 
-                        {identify != ""? <Text style={styles.textSubTitle}>How to identify:</Text>:null}
-                            {identify != ""? <Text style={styles.textNormal}>{identify}</Text>:null}
+                    {identify != ""? <Text style={styles.textSubTitle}>How to identify:</Text>:null}
+                        {identify != ""? <Text style={styles.textNormal}>{identify}</Text>:null}
 
-                        {curiostity != ""? <Text style={styles.textSubTitle}>Curiosities:</Text>:null}
-                                {curiostity != ""? <Text style={styles.textNormal}>{curiostity}</Text>:null}
+                    {curiostity != ""? <Text style={styles.textSubTitle}>Curiosities:</Text>:null}
+                            {curiostity != ""? <Text style={styles.textNormal}>{curiostity}</Text>:null}
 
-                        {length != ""? <Text style={styles.textSubTitle}>Average Length:</Text>:null}
-                            {length != ""? <Text style={styles.textNormal}>{length}</Text>:null}
+                    {length != ""? <Text style={styles.textSubTitle}>Average Length:</Text>:null}
+                        {length != ""? <Text style={styles.textNormal}>{length}</Text>:null}
 
-                            {weight != ""? <Text style={styles.textSubTitle}>Average Weight:</Text>:null}
-                            {weight != ""?<Text style={styles.textNormal}>{weight}</Text>:null}
+                    {weight != ""? <Text style={styles.textSubTitle}>Average Weight:</Text>:null}
+                        {weight != ""?<Text style={styles.textNormal}>{weight}</Text>:null}
 
-                        {color != ""? <Text style={styles.textSubTitle}>Color:</Text>:null}
-                            {color != ""? <Text style={styles.textNormal}>{color}</Text>:null}
+                    {color != ""? <Text style={styles.textSubTitle}>Color:</Text>:null}
+                        {color != ""? <Text style={styles.textNormal}>{color}</Text>:null}
 
                 </ScrollView>
             </ImageBackground>                
