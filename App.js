5966dbfbc4a6de44375b355fb7416d7c16ecd2bb
Marcos <marcos.dss92@gmail.com>
2021-12-26 18:37:19 +0000

    added images

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import OthersScreen from './src/screens/OthersScreen';
 import SelectPictureScreen from './src/screens/SelectPictureScreen';
 import Whale1 from './src/screens/Whale1';
 import { AddTripScreenNavigator, HomeScreenNavigator, SpeciesScreenNavigator, PostCardNavigator } from './src/screens/CustomNavigation';
+import TripListScreen from './src/screens/TripListScreen';
 
 
 const Tab = createBottomTabNavigator();
@@ -95,6 +96,7 @@ export default () => (
         <Tab.Screen name="Species" component={SpeciesScreenNavigator} options={{tabBarIcon: ({size,color}) => (<Feather name="search" size={size} color={color} /> )}}/>
         <Tab.Screen name="Add Trip" component={AddTripScreenNavigator} options={{tabBarIcon: ({size,color}) => (<Ionicons name="add-circle" size={size} color={color} /> )}}/>
         <Tab.Screen name="Testing" component={PostCardNavigator} options={{tabBarIcon: ({size,color}) => (<Ionicons name="add-circle" size={size} color={color} /> )}}/>
+        <Tab.Screen name="Trip" component={TripListScreen} options={{tabBarIcon: ({size,color}) => (<Ionicons name="add-circle" size={size} color={color} /> )}}/>
       </Tab.Navigator>
     </NavigationContainer>
 
---
+++ src/screens/TripListScreen.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import {View, Text, Button, ScrollView, StyleSheet, Image, TouchableOpacity, Dimensions, ImageBackground} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+let ITEM_WIDTH = Dimensions.get('window').width;
+
+export default function TripListScreen({ navigation }){
+    console.log("I'm in SpeciesScreen");
+    
+    return(
+        <View style ={styles.container}>
+            <ImageBackground blurRadius = {50} source={require("../../assets/Whales/1Blainvilles_beaked_whale.jpg")} resizeMode="cover" style={styles.imageBackground}> 
+                <ScrollView> 
+                    <TouchableOpacity onPress={() => navigation.navigate("Whales")}>       
+                        <View style ={styles.item}>                    
+                            <Image style = {styles.image} source={require("../../assets/Whales/10Killer_whale_or_Orca.jpg") }/>
+                                <Text style = {styles.textSubTitle}>Whales</Text>
+                        </View>
+                    </TouchableOpacity>
+
+
+                    
+                                            
+                </ScrollView>
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //alignItems: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',      
+    },
+    item: {
+        marginTop: 15,
+        padding:5,
+        flex:1,
+        fontSize: 20,
+        
+    },
+    textSubTitle: {
+        position:'absolute',
+        color: 'white',
+        fontSize: 20,
+        marginHorizontal: 10,
+        marginVertical: 25,
+        marginLeft: 30,
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowRadius: 8
+    },
+    image: {
+        marginTop: 15,
+        marginLeft: 15,
+        flex: 1,
+        width: ITEM_WIDTH - 35,
+        height: ITEM_WIDTH/1.5,
+        borderRadius: 15,
+    },
+    imageBackground: {
+        flex: 1
+    },
+})
