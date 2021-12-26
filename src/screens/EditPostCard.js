14af0337b93d57387d515acb217c13d438c83a5d
Francisc546 <49655530+Francisc546@users.noreply.github.com>
2021-12-26 18:56:22 +0000

    Adicionado novo rating no editpostcard

--- src/screens/EditPostCard.js
+++ src/screens/EditPostCard.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Text} from 'react-native';
 
-function EditPostCard(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+
+import  Rating from 'react-native-easy-rating';
+
+export default function EditPostCard() {
+  const [rating,setRating] = useState();
+
+  return (
+      <View>
+    <Rating
+      rating={rating}
+      max={5}
+      iconWidth={24}
+      iconHeight={24}
+      onRate={setRating}
+      />
+      <Text>{rating}</Text>
+      </View>
+  );
+  
 }
-
-export default EditPostCard;
