99ce4f205fc30464de47f0fbc66fa6a38bfc5edd
jlpmartins98 <50104175+jlpmartins98@users.noreply.github.com>
2021-12-26 18:46:21 +0000

    blank lines deleted and code formated

--- src/screens/AddTripScreen.js
+++ src/screens/AddTripScreen.js
@@ -40,46 +40,35 @@ const AddTripScreen = ({ navigation }) => {
 
     return (
 
-        <ImageBackground blurRadius = {3} source={require("../../assets/trip.jpg")} resizeMode="cover" style={styles.imageBackground}>
-        <View style={styles.container}>
-            <Text style={styles.textSubTitle}>Type the code of your trip</Text>
-            
-            <TextInput
-                style={{ padding: 10, height: 40, width: 150, margin: 15 }}
-                backgroundColor='#FFFFFF'
-                //se o input for números onChangeText = {onChangeNumber} + value = {number} + keyboardType="numeric" 
-                placeholder="Type Trip Code Here"
-                //onSubmitEditing={text => setText(text)}
-                onChangeText={text => setText(text)}
-                defaultValue={text}
-            />
+        <ImageBackground blurRadius={3} source={require("../../assets/trip.jpg")} resizeMode="cover" style={styles.imageBackground}>
+            <View style={styles.container}>
+                <Text style={styles.textSubTitle}>Type the code of your trip</Text>
 
-            <Button
-                title="Add Trip"
-                onPress={() => { checkTripTime() }}
+                <TextInput
+                    style={{ padding: 10, height: 40, width: 150, margin: 15 }}
+                    backgroundColor='#FFFFFF'
+                    //se o input for números onChangeText = {onChangeNumber} + value = {number} + keyboardType="numeric" 
+                    placeholder="Type Trip Code Here"
+                    //onSubmitEditing={text => setText(text)}
+                    onChangeText={text => setText(text)}
+                    defaultValue={text}
+                />
 
-            />
-           
-            
-        </View>
+                <Button
+                    title="Add Trip"
+                    onPress={() => { checkTripTime() }}
+                />
+            </View>
         </ImageBackground>
 
-
-
-
-
-
-
-
     );
-
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-  
+
 
 
     },
@@ -92,7 +81,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         fontSize: 30,
-        marginTop: 50,textShadowColor: 'rgba(0, 0, 0, 1)',
+        marginTop: 50, textShadowColor: 'rgba(0, 0, 0, 1)',
         textShadowRadius: 8
 
     },
